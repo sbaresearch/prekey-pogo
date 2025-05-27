6747aa2685c8b29c1ec4ada1b18f518007ad8c16
Gabriel Gegenhuber <ggegenhuber@sba-research.org>
2025-05-27 12:51:16 +0200

    disalbe device cache

--- setup.sh
+++ setup.sh
@@ -8,3 +8,4 @@ git clone --filter=blob:none https://github.com/tulir/whatsmeow.git
 go mod edit -replace go.mau.fi/whatsmeow=./whatsmeow
 
 git -C whatsmeow/ apply ../add_prekey_timestamp.patch
+git -C whatsmeow/ apply ../disable_device_cache.patch
